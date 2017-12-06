8401567ba33011593833d966f7a9775b632a232c
Roy Montoya <roymontoya89@gmail.com>
2017-12-06 12:53:49 -0600

    initial commit + finished example

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Reflux from 'reflux'
+import AppStore from './stores/AppStore'
+import Actions from './actions/Actions'
+import './App.css';
+
+class App extends Reflux.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+    this.store = AppStore;
+    this.storeKeys = ['count'];
+  }
+  render() {
+    return (<div className="App">
+      <h1>Contador</h1>
+      <label className="App-label">{this.state.count}</label><br/>
+      <div>
+        <button onClick={() => {Actions.addOne();}}>Agregar</button>
+        <button onClick={() => {Actions.minusOne();}} >Restar</button>
+      </div>
+    </div>);
+  }
+}
+
+export default App;
